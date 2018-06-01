d19b4a1ce145a955524158f6b6f245d89929b98c
Igor <inngvar@yandex.ru>
2018-06-01 16:50:19 +0300

    #7 Рефакторинг определения jdl типа

--- src/test/java/com/sunrise/jdl/generator/EntitiesServiceTest.java
+++ src/test/java/com/sunrise/jdl/generator/EntitiesServiceTest.java
@@ -12,6 +12,8 @@ import java.util.List;
 
 public class EntitiesServiceTest {
 
+    private EntitiesService entitiesService = new EntitiesService();
+
     /**
      * TODO я думаю нужен тест на небольшом колличенстве данных, 1-2 сущности, что бы проверить, что все поля устонавливаются правильно.
      */
@@ -20,21 +22,21 @@ public class EntitiesServiceTest {
         ArrayList<InputStream> streams = new ArrayList<>(2);
         streams.add(this.getClass().getResourceAsStream("/dictionary.csv"));
         streams.add(this.getClass().getResourceAsStream("/data.csv"));
-        EntitiesService reader = new EntitiesService(streams);
-        List<Entity> result = reader.readAll();
+        EntitiesService reader = new EntitiesService();
+        List<Entity> result = reader.readAll(streams);
         Assert.assertNotNull(result);
         Assert.assertTrue(result.size() > 0);
     }
 
+    /**
+     * На прмере чтения сущности ContactDataCorrection и DriverLicense
+     */
     @Test
-    // На прмере чтения сущности ContactDataCorrection и DriverLicense
     public void validationOfFieldsTypeReading() {
         ArrayList<InputStream> streams = new ArrayList<>(1);
         streams.add(this.getClass().getResourceAsStream("/twoEntities.csv"));
-        EntitiesService reader = new EntitiesService(streams);
-        List<Entity> result = reader.readAll();
+        List<Entity> result = entitiesService.readAll(streams);
         Entity contacts = result.get(0);
-        reader.correctsFieldsType(contacts);
         ArrayList<Field> contactsFields = contacts.getFields();
 
         Assert.assertNotNull(result);
@@ -49,15 +51,14 @@ public class EntitiesServiceTest {
 
         Entity license = result.get(1);
         ArrayList<Field> licenseFields = license.getFields();
-        reader.correctsFieldsType(license);
 
-        Assert.assertEquals("Дата/время", licenseFields.get(0).getFieldType());
+        Assert.assertEquals("Instant", licenseFields.get(0).getFieldType());
         Assert.assertEquals("String", licenseFields.get(1).getFieldType());
         Assert.assertEquals("String", licenseFields.get(2).getFieldType());
         Assert.assertEquals("String", licenseFields.get(3).getFieldType());
         Assert.assertEquals("String", licenseFields.get(4).getFieldType());
-        Assert.assertEquals("Дата/время", licenseFields.get(5).getFieldType());
-        Assert.assertEquals("Дата/время", licenseFields.get(6).getFieldType());
+        Assert.assertEquals("Instant", licenseFields.get(5).getFieldType());
+        Assert.assertEquals("Instant", licenseFields.get(6).getFieldType());
         Assert.assertEquals("String", licenseFields.get(7).getFieldType());
         Assert.assertEquals("Employee", licenseFields.get(8).getFieldType());
         Assert.assertEquals("Group", licenseFields.get(9).getFieldType());
@@ -68,11 +69,9 @@ public class EntitiesServiceTest {
     public void testCorrectFieldsType() {
         ArrayList<InputStream> streams = new ArrayList<>(1);
         streams.add(this.getClass().getResourceAsStream("/twoEntities.csv"));
-        EntitiesService entitiesService = new EntitiesService(streams);
-        List<Entity> result = entitiesService.readAll();
+        List<Entity> result = entitiesService.readAll(streams);
         Entity contacts = result.get(0);
         ArrayList<Field> contactsFields = contacts.getFields();
-        int numberOfCorreciton = entitiesService.correctsFieldsType(result);
         
         Assert.assertNotNull(result);
         Assert.assertEquals("Address", contactsFields.get(0).getFieldType());
@@ -86,7 +85,6 @@ public class EntitiesServiceTest {
 
         Entity license = result.get(1);
         ArrayList<Field> licenseFields = license.getFields();
-        entitiesService.correctsFieldsType(license);
 
         Assert.assertEquals("Instant", licenseFields.get(0).getFieldType());
         Assert.assertEquals("String", licenseFields.get(1).getFieldType());
@@ -99,7 +97,7 @@ public class EntitiesServiceTest {
         Assert.assertEquals("Employee", licenseFields.get(8).getFieldType());
         Assert.assertEquals("Group", licenseFields.get(9).getFieldType());
         Assert.assertEquals("Список<Document>", licenseFields.get(10).getFieldType());
-        Assert.assertEquals(9, numberOfCorreciton);
+
     }
 
     /**
@@ -109,20 +107,8 @@ public class EntitiesServiceTest {
     public void testWriteToFile() {
         ArrayList<InputStream> streams = new ArrayList<>(2);
         streams.add(this.getClass().getResourceAsStream("/twoEntities.csv"));
-        EntitiesService entitiesService = new EntitiesService(streams);
-        List<Entity> result = entitiesService.readAll();
-
-        entitiesService.correctsFieldsType(result);
-        entitiesService.checkIsFieldSupportedInJDL(result);
+        List<Entity> result = entitiesService.readAll(streams);
         int numberOfStructure = entitiesService.createStructure(result);
         Assert.assertEquals(10, numberOfStructure);
-
-//        try (BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\work\\jdlGenerator\\src\\main\\java\\com\\sunrise\\jdl\\generator\\result\\result2.txt"))) {
-//            entitiesService.writeEntityToFile(result, writer);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-
     }
 }
