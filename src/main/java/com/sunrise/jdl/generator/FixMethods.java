44d02d29fb4303394099ef0840592f990fa5008f
krovyaka <krovyaka1@yandex.ru>
2018-11-29 16:41:28 +0300

    #846

--- src/main/java/com/sunrise/jdl/generator/FixMethods.java
+++ src/main/java/com/sunrise/jdl/generator/FixMethods.java
@@ -5,6 +5,7 @@ import com.sunrise.jdl.generator.entities.Field;
 import com.sunrise.jdl.generator.entities.LinkedField;
 import com.sunrise.jdl.generator.ui.ProjectionInfo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FixMethods {
@@ -12,34 +13,24 @@ public class FixMethods {
     public static void fixListProjections(ProjectionInfo projectionInfo) {
         if (projectionInfo.getCode() == null) return;
         if (projectionInfo.getCode().equals("newListProjection")) {
-            Action edit = new Action();
-            edit.setCode("edit");
-            edit.setDisplayType("tableMenu");
-            edit.setGroup("edit");
-            edit.setStyle("editBtn");
-
             List<Action> actions = projectionInfo.getActions();
-
-            actions.set(1,edit);
-            
-            actions.set(2, new Action());
-            actions.get(2).setCode("delete");
-            actions.get(2).setStyle("discardBtn");
-            actions.get(2).setGroup("edit");
-            actions.get(2).setDisplayType("tableMenu");
-
+            actions.set(1,new Action("tableMenu","edit","editBtn","edit"));
+            actions.set(2, new Action("tableMenu","edit","discardBtn","delete"));
             actions.remove(6);
             actions.remove(5);
             actions.remove(4);
             actions.remove(3);
         }
+
+        if (projectionInfo.getCode().equals("documentViewListProjection")) {
+            projectionInfo.setActions(new ArrayList<>());
+            projectionInfo.getActions().add(new Action("tableMenu","other","pathBtn","path"));
+            projectionInfo.getActions().add(new Action("tableMenu","info","cardBtn","card"));
+            projectionInfo.getActions().add(new Action("tableMenu","info","printBtn","print"));
+        }
+
         if (projectionInfo.getCode().equals("archiveListProjection")) {
-            Action fromArchive = new Action();
-            fromArchive.setCode("fromArchive");
-            fromArchive.setDisplayType("tableMenu");
-            fromArchive.setGroup("connect");
-            fromArchive.setStyle("fromArchiveBtn");
-            projectionInfo.getActions().set(3,fromArchive);
+            projectionInfo.getActions().set(3,new Action("tableMenu","connect","fromArchiveBtn","fromArchive"));
         }
         projectionInfo.setCode(projectionInfo.getCode().replaceAll("ListProjection", ""));
     }
