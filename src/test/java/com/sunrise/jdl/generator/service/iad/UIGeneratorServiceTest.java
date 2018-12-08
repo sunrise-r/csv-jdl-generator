6630365c9abb9e9f2fd7abc8b6fc65e30c77f93d
igor.ch <igor.ch@sunrise-r.ru>
2018-12-08 11:09:37 +0300

    #ISSUE-6 test fixed

--- src/test/java/com/sunrise/jdl/generator/service/iad/UIGeneratorServiceTest.java
+++ src/test/java/com/sunrise/jdl/generator/service/iad/UIGeneratorServiceTest.java
@@ -4,7 +4,7 @@ import com.sunrise.jdl.generator.ui.RegistryItem;
 import com.sunrise.jdl.generator.ui.UIGenerateParameters;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class UIGeneratorServiceTest {
 
@@ -13,13 +13,14 @@ public class UIGeneratorServiceTest {
     @Test
     public void createPresentationFor() {
         String registryName = "registry";
-        String entityName="entity";
-        UIGenerateParameters params =  new UIGenerateParameters();
+        String entityName = "entity";
+        UIGenerateParameters params = new UIGenerateParameters();
         params.setPluralTranslations(false);
         params.setTranslationPath("");
-        RegistryItem item = uiGeneratorService.createPresentationFor(entityName,registryName, params);
-        assertEquals(entityName+"Presentation",item.getCode());
-        assertEquals(entityName+"Presentation",item.getLabel());
-        assertEquals(registryName,item.getParentCode());
+        params.setTitlePath("partnerGatewayApp.partnerDocuments.<ENTITY_NAME>.home.title");
+        RegistryItem item = uiGeneratorService.createPresentationFor(entityName, registryName, params);
+        assertEquals(entityName + "Presentation", item.getCode());
+        assertEquals("partnerGatewayApp.partnerDocuments.entity.home.title", item.getLabel());
+        assertEquals(registryName, item.getParentCode());
     }
 }
