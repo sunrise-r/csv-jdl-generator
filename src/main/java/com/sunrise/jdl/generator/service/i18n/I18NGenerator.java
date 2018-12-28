fa46fecceee935611e304dcc79cb06d96d295b50
igor.ch <igor.ch@sunrise-r.ru>
2018-12-28 22:42:27 +0300

    #ISSUE-14 i18n for relations added

--- src/main/java/com/sunrise/jdl/generator/service/i18n/I18NGenerator.java
+++ src/main/java/com/sunrise/jdl/generator/service/i18n/I18NGenerator.java
@@ -4,6 +4,7 @@ import com.sunrise.jdl.generator.config.JdlConfig;
 import com.sunrise.jdl.generator.entities.RawData;
 import com.sunrise.jdl.generator.service.common.GeneratorWriter;
 import com.sunrise.jdl.generator.service.jdl.JdlEntity;
+import com.sunrise.jdl.generator.service.jdl.JdlRelation;
 
 import java.io.File;
 import java.nio.file.Paths;
@@ -18,15 +19,14 @@ public class I18NGenerator {
         this.i18NService = new I18NService(writer);
     }
 
-    public void generate(String appName, File resourceFolder, Map<String, List<RawData>> rawData, List<JdlEntity> entities) {
-        List<I18nModel> i18nModel = i18NService.generateModel(appName, rawData, entities);
+    public void generate(String appName, File resourceFolder, Map<String, List<RawData>> rawData, List<JdlEntity> entities, List<JdlRelation> relations) {
+        List<I18nModel> i18nModel = i18NService.generateModel(appName, rawData, entities, relations);
         i18nModel.stream().forEach(model -> i18NService.saveModel(resourceFolder, model));
     }
 
-    public void generate(JdlConfig jdlConfig, Map<String, List<RawData>> rawData, List<JdlEntity> entities) {
+    public void generate(JdlConfig jdlConfig, Map<String, List<RawData>> rawData, List<JdlEntity> entities, List<JdlRelation> relations) {
         String appName = jdlConfig.getGateway();
         File resourceFolder = Paths.get(jdlConfig.getTargetResourceFolder()).toFile();
-        this.generate(appName, resourceFolder, rawData, entities);
-
+        this.generate(appName, resourceFolder, rawData, entities, relations);
     }
 }
